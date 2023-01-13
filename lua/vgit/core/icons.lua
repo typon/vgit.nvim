20fb9c40d7d51eeab88272be69805807a2ead0de
hfarooq <hfarooq@covariant.ai>
2023-01-13 12:43:07 -0500

    remove icons

--- lua/vgit/core/icons.lua
+++ lua/vgit/core/icons.lua
@@ -1,13 +1,7 @@
-local has_web_devicons, web_devicons = pcall(require, 'nvim-web-devicons')
-
 local icons = {}
 
 function icons.get(fname, extension)
-  if not has_web_devicons or not web_devicons.has_loaded() then
-    return nil, ''
-  end
-
-  return web_devicons.get_icon(fname, extension)
+  return nil, ''
 end
 
 return icons
